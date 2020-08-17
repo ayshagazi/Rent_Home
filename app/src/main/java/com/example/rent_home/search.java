b2a42ec8b5f52cb3f881b5e9339762d0df612267
ptsourav21 <ptsourav21@gmail.com>
2020-08-17 22:58:13 +0600

    Purnendu

--- app/src/main/java/com/example/rent_home/search.java
+++ app/src/main/java/com/example/rent_home/search.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class search extends AppCompatActivity {
 
+    String[] DivisionsStringVariable;
+    String[] SylhetDivisionDistrictStringVariable;
+    String[] DhakaDivisionDistrictStringVariable;
+
+    String SelectedDivision;
+    private TextView text;
+    private Spinner DivisionSpinnerVariable;
+    private Spinner DistrictSpinnerVariable;
+    boolean isActive;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +56,50 @@ public class search extends AppCompatActivity {
                 return false;
             }
         });
+
+        DivisionsStringVariable=getResources().getStringArray(R.array.DivisionsString);// ei variable e values er declare kora string recieve korbe
+        DhakaDivisionDistrictStringVariable=getResources().getStringArray(R.array.DhakaDivisionsDistrictsString);//same
+        SylhetDivisionDistrictStringVariable=getResources().getStringArray(R.array.SylhetDivisionDistrictString);//same
+
+        DivisionSpinnerVariable=(Spinner) findViewById(R.id.spinnerDivison); // divison spinner jeta activity_search.xml e ase oita ke variable e set korbe
+        DistrictSpinnerVariable =(Spinner) findViewById(R.id.spinnerDistrict);//same
+
+        ArrayAdapter<String> DivisionAdapter = new ArrayAdapter<String>(this, R.layout.spinnerdisplay, R.id.spinnerDisplay, DivisionsStringVariable);// ei adapter division er nam gula ke spinner display layout er maddome adapter e set korbe
+
+
+        DivisionSpinnerVariable.setAdapter(DivisionAdapter);// set kora divison gulu spinner e show korbe
+
+        refreshContent();
+
     }
+    public void refreshContent(){
+
+        SelectedDivision=DivisionSpinnerVariable.getSelectedItem().toString();
+        ArrayAdapter<String> SylhetDivisionAdapter = new ArrayAdapter<String>(this, R.layout.spinnerdisplay, R.id.spinnerDisplay, SylhetDivisionDistrictStringVariable);//same
+        ArrayAdapter<String> DhakaDivisionAdapter = new ArrayAdapter<String>(this, R.layout.spinnerdisplay, R.id.spinnerDisplay, DhakaDivisionDistrictStringVariable);//same
+        if(SelectedDivision.equals("Sylhet")) {
+            DistrictSpinnerVariable.setAdapter(SylhetDivisionAdapter);
+        }else if(SelectedDivision.equals("Dhaka"))
+        {
+            DistrictSpinnerVariable.setAdapter(DhakaDivisionAdapter);
+        }
+        text=(TextView)findViewById(R.id.textView10);
+        text.setText(SelectedDivision);
+
+        refresh(500);
+    }
+
+    private void refresh(int milliseconds) {
+        final Handler handler = new Handler();
+
+        final Runnable runnable= new Runnable() {
+            @Override
+            public void run() {
+                refreshContent();
+            }
+
+        };
+        handler.postDelayed(runnable, milliseconds);
+    }
+
 }
